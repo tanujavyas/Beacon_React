630c137175ff9a84f2cbf6545659690b630d448c
Snehal Kale <snehal.kale@eternussolutions.com>
2019-06-10 18:49:56 +0530

    lab attribute test

--- src/routes/laboratory/laboratoryForm/laboratoryForm.js
+++ src/routes/laboratory/laboratoryForm/laboratoryForm.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
+const attributeList = require("./branchAttribute.json");
 
 export default class LaboratoryForm extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    attributeList.forEach(attr => {
+      let attributeName = attr.attributeName;
+      this.setState({});
+    }, this);
+  }
+
   render() {
     return (
       <div>
